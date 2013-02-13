21e24559169a5ee35ff1daffe25373ac68a660d6
Taylor Trimble <t.trim@me.com>
2013-02-12 21:30:49 -0600

    rails new

---
+++ vendor/README.md
@@ -0,0 +1,4 @@
+teslaworks.net
+==============
+
+New web app for the University of Minnesota Tesla Works student organization
---
+++ vendor/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Teslaworks::Application.config.secret_token = '1a42f300086bf6c2947e2344d82281329ad0132f93e5f8b67593c7d2183f189fda883409b76efd29583452939bf1fc9fed4b779d674a98a543aca0ff50a6e38d'
