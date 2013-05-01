9cbbc7351f1271b374707942f823d992ca219fbe
Matthew Lewis <matt@mplewis.com>
2013-05-01 16:21:00 -0500

    reads from json

--- server_dev.py
+++ server_dev.py
@@ -1,9 +1,11 @@
+import projects
 from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', projects=[{"name": "testproj"}, {"name": "alphagamma"}])
+    project_list = projects.get_projects()
+    return render_template('index.html', projects=project_list)
 
 if __name__ == '__main__':
     app.run(debug=True)
