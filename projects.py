1a8fb9590ea02f703c1494497c464b248e904362
Taylor Trimble <t.trim@me.com>
2013-06-16 23:57:27 -0700

    Implement directory crawl to build projects dict

--- projects.py
+++ projects.py
@@ -1,9 +1,15 @@
 import json
+import os
 
-PROJECT_JSON_LOC = 'projects.json'
+PROJECT_JSON_LOC = 'data/projects'
 
 def get_projects():
-	with open(PROJECT_JSON_LOC, 'r') as f:
-		data = json.load(f)
-	project_list = data['projects']
-	return project_list
+    projects = {}
+    
+    for file_name in os.listdir(PROJECT_JSON_LOC):
+        long_file_name = os.path.join(PROJECT_JSON_LOC, file_name)
+        with open(long_file_name, 'r') as file:
+            data = json.load(file)
+        projects[file_name] = data
+    
+    return projects
