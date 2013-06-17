008491ec0c9277f6d25b156e61ab022eb158fc00
Taylor Trimble <t.trim@me.com>
2013-06-16 19:10:57 -0700

    Initial easy routes

--- server_dev.py
+++ server_dev.py
@@ -8,5 +8,14 @@ def index():
     project_list = projects.get_projects()
     return render_template('index.html', projects=project_list)
 
+@app.route('/blog')
+def blog():
+    return "Flasktopress isn't quite ready yet, but we're stoked that it's coming."
+
+@app.route('/<project>')
+def project(project):
+    return "Bet you can't wait to join %s, huh?" % project
+
+
 if __name__ == '__main__':
     app.run(debug=True)
